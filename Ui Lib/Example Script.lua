4461af1142e9ad632449bdbce48b10cdf1f0fc7c
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2024-06-04 22:35:38 +0300

    Update Example Script.lua

--- Ui Lib/Example Script.lua
+++ Ui Lib/Example Script.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ShaddowScripts/Main/main/Library"))()
+local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/qqwizzixxxx/UntitledHub-WarTycoon/main/Ui%20Lib/Shadow%20Lib.lua"))()
 
 local Main = library:CreateWindow("Main","Crimson")
 
